8759753c6b1e04a859bfbd171aeed1e16eaea853
Pushpendra Singh <pushpendrathakur9968@gmail.com>
2024-05-09 07:49:59 +0530

    second

---
+++ first.py
@@ -0,0 +1,12 @@
+#a = input("Enter First Number: ")
+#b = input("Enter Second Number:")
+#sum = int(a) + int(b)
+#print(sum)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
